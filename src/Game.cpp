1df8353ab3ec35cdc644670461ec135efc0ff18c
farouk khaled <faroukkhaled2711@gmail.com>
2022-04-22 18:17:23 +0200

    create the logic behind the start and render the update the game

--- src/Game.cpp
+++ src/Game.cpp
@@ -68,3 +68,85 @@ NODE Game::deleteTail(NODE tail)
 	tail = NULL;
 	return temp;
 }
+
+/*
+
+Describtion :inside this function we update all game
+1- if the train git out of the window return it back
+2- hadle if the train touch the snoopy
+3-handle the motion by is=nsert at head and delete at tail
+4- check if the train intersect with it self to teminate the game
+input : no
+output : void
+
+ */
+void Game::Update()
+{
+	// return the train back to window
+	if (head->posx > 810)
+		head->posx = 0;
+	if (head->posx < 0)
+		head->posx = 810;
+	if (head->posy > 600)
+		head->posy = 0;
+	if (head->posy < 0)
+		head->posy = 600;
+	HandleSnoopyTouched();
+	// call this function every frame
+	head = insertHead(head, &TrainSprite);
+	tail = deleteTail(tail);
+	checkIntersection(head, &window);
+	count = 0;
+}
+/*
+
+Describtion :the steps
+1- is to use sfml tool to load the sprite entity
+2- create head node
+3- intilaze the postin 0,0 and the direction to right
+4- and the tail is the head initially
+5-set the snoopy postion
+
+input :
+output :
+
+ */
+void Game::Start()
+{
+	LoadSprites();
+	head = (NODE)malloc(sizeof(struct node));
+	head->posx = 0;
+	head->posy = 0;
+	head->direction = right;
+	head->s = &TrainSprite;
+	head->llink = head->rlink = NULL;
+	tail = head;
+	snoopy.setPosition(head->posx, head->posy);
+	Score.setPosition(700, 10);
+	Score.setString(sf::String(std::to_string(score + 1)));
+	window.create(sf::VideoMode(800, 600), "Train", sf::Style::Titlebar | sf::Style::Close);
+}
+
+/*
+
+Describtion :
+call the function start we explin it
+and the render we will exaplin but it is related to the window and sfml
+input :
+output :
+
+ */
+void Game::Run()
+{
+	Start();
+	while (window.isOpen())
+	{
+		Render();
+		processEvents();
+		if (count == speed)
+		{
+			Update();
+		}
+		count++;
+	}
+}
